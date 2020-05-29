6eebc8fe3667269c79cdebc99550d4558ea1a84e
brianokinyi <brianokinyi.bo@gmail.com>
2020-05-29 15:40:26 +0300

    Add login register logic

---
+++ app/Http/Controllers/Api/LoginController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Foundation\Auth\AuthenticatesUsers;
+
+class LoginController extends Controller
+{
+    use AuthenticatesUsers;
+
+    /**
+    * Login user and generate API token
+    *
+    * @param Request $request
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function login(Request $request){
+        $this->validateLogin($request);
+
+        if ($this->attemptLogin($request)) {
+            $user = $this->guard()->user();
+            $token =  $user->createToken('Subletor')->accessToken;
+
+            return response()->json([
+                'authentication' => [
+                    'token' => $token
+                ]
+            ], 200);
+        }
+
+        return $this->sendFailedLoginResponse($request);
+   }
+
+    /**
+    * Logout user and invalidate API token
+    *
+    * @param Request $request
+    * @return \Illuminate\Http\JsonResponse
+    */
+    public function logout(Request $request)
+    {
+        $request->user()->token()->revoke();
+
+        return response()->json([
+            'message' => "User logged out"
+        ], 200);
+    }
+}
---
+++ app/Http/Controllers/Api/RegisterController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers\Api\Auth;
+
+use App\Http\Controllers\Controller;
+use App\Models\User;
+use Illuminate\Foundation\Auth\RegistersUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Auth\Events\Registered;
+
+class RegisterController extends Controller
+{
+    use RegistersUsers;
+
+    /**
+     * Register user and generate API token.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  mixed  $user
+     * @return mixed
+     */
+    protected function registered(Request $request, $user)
+    {
+        $token =  $user->createToken('Subletor')->accessToken;
+
+        return response()->json([
+            'authentication' => [
+                'token' => $token
+            ],
+            'user' => $user
+        ], 201);
+    }
+
+
+    /**
+     * Get a validator for an incoming registration request.
+     *
+     * @param  array  $data
+     * @return \Illuminate\Contracts\Validation\Validator
+     */
+    protected function validator(array $data)
+    {
+        return Validator::make($data, [
+            'name' => ['required', 'string'],
+            'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
+            'password' => ['required', 'string', 'min:6', 'confirmed']
+        ]);
+    }
+
+
+    /**
+     * Create a new user instance after a valid registration.
+     *
+     * @param  array  $data
+     * @return \App\User
+     */
+    protected function create(array $data)
+    {
+        return  User::create([
+            'name' => $data['name'],
+            'email' => $data['email'],
+            'password' => Hash::make($data['password']),
+        ]);
+    }
+}
