47c45c1d007a276c9493fbc5e02e15f3261c429e
wemali05 <alfredwemali05@gmail.com>
2020-07-18 22:49:05 +0300

    remove auth for show api to work

--- routes/api.php
+++ routes/api.php
@@ -20,9 +20,9 @@ Route::post('/token', 'Auth\LoginController@getToken');
   //videos
   Route::get('videos', 'Api\VideosController@index');
 //   Route::get('videos/{video}', 'Api\VideosController@show');
-Route::middleware(['auth:api'])->group(function () {
+// Route::middleware(['auth:api'])->group(function () {
     Route::apiResource('/videos', 'Api\VideosController')->except('index');
-});
+// });
 
 //   Route::middleware(['auth:api'])->group(function () {
 //       Route::apiResource('/videos', 'Api\VideosController')->except('index');
