fd00280c69a1f1eafa1baea80046ba435de7a0b7
wemali05 <alfredwemali05@gmail.com>
2020-07-18 22:14:29 +0300

    user resource

--- app/Http/Resources/VideosResource.php
+++ app/Http/Resources/VideosResource.php
@@ -30,6 +30,7 @@ class VideosResource extends JsonResource
             'tags' => $this->tags,
             'created_at' => $this->created_at,
             'updated_at' => $this->updated_at,
+            // 'user' => new UserResource($this->user)
         ];
     }
 }
