33f4cf3d7b7012cb30d764575e559d61ba90acad
wemali05 <alfredwemali05@gmail.com>
2020-07-19 10:51:59 +0300

    protect from mass assignment

--- app/Models/Interest.php
+++ app/Models/Interest.php
@@ -6,7 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Interest extends Model
 {
-    //
+    protected $fillable = [
+       'title', 'description',
+    ];
 
     public function user()
     {
