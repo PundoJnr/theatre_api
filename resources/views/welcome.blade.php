f44e03e688e7bccd25193697f2ff6f64f801432c
brianokinyi <brianokinyi.bo@gmail.com>
2020-08-15 19:40:23 +0300

    Add coming soon page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,100 +1,143 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang="en">
 
-        <title>Laravel</title>
+<head>
+    <title>Theatre.co.ke - Coming Soon</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!--===============================================================================================-->
+    <link rel="icon" type="image/png" href="images/icons/favicon.ico" />
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="fonts/font-awesome-4.7.0/css/font-awesome.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/animate/animate.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="vendor/select2/select2.min.css">
+    <!--===============================================================================================-->
+    <link rel="stylesheet" type="text/css" href="css/util.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!--===============================================================================================-->
+</head>
 
-        <!-- Fonts -->
-        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+<body>
 
-        <!-- Styles -->
-        <style>
-            html, body {
-                background-color: #fff;
-                color: #636b6f;
-                font-family: 'Nunito', sans-serif;
-                font-weight: 200;
-                height: 100vh;
-                margin: 0;
-            }
 
-            .full-height {
-                height: 100vh;
-            }
-
-            .flex-center {
-                align-items: center;
-                display: flex;
-                justify-content: center;
-            }
-
-            .position-ref {
-                position: relative;
-            }
-
-            .top-right {
-                position: absolute;
-                right: 10px;
-                top: 18px;
-            }
-
-            .content {
-                text-align: center;
-            }
-
-            .title {
-                font-size: 84px;
-            }
-
-            .links > a {
-                color: #636b6f;
-                padding: 0 25px;
-                font-size: 13px;
-                font-weight: 600;
-                letter-spacing: .1rem;
-                text-decoration: none;
-                text-transform: uppercase;
-            }
-
-            .m-b-md {
-                margin-bottom: 30px;
-            }
-        </style>
-    </head>
-    <body>
-        <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="content">
-                <div class="title m-b-md">
-                    Laravel
+    <div class="size1 bg0 where1-parent">
+        <!-- Coutdown -->
+        <div class="flex-c-m bg-img1 size2 where1 overlay1 where2 respon2"
+            style="background-image: url('images/bg01.jpeg');">
+            <div class="wsize2 flex-w flex-c-m cd100 js-tilt">
+                <div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+                    <span class="l2-txt1 p-b-9 days">14</span>
+                    <span class="s2-txt4">Days</span>
                 </div>
 
-                <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                <div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+                    <span class="l2-txt1 p-b-9 hours">23</span>
+                    <span class="s2-txt4">Hours</span>
+                </div>
+
+                <div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+                    <span class="l2-txt1 p-b-9 minutes">50</span>
+                    <span class="s2-txt4">Minutes</span>
+                </div>
+
+                <div class="flex-col-c-m size6 bor2 m-l-10 m-r-10 m-t-15">
+                    <span class="l2-txt1 p-b-9 seconds">39</span>
+                    <span class="s2-txt4">Seconds</span>
                 </div>
             </div>
         </div>
-    </body>
+
+        <!-- Form -->
+        <div class="size3 flex-col-sb flex-w p-l-75 p-r-75 p-t-45 p-b-45 respon1">
+            <div class="wrap-pic1">
+                <img src="images/icons/logo.png" alt="LOGO" width="50%" height="50%">
+            </div>
+
+            <div class="p-t-50 p-b-60">
+                <p class="m1-txt1 p-b-36">
+                    Theatre is <span class="m1-txt2">Coming Soon</span>, subscribe to be the first to know!
+                </p>
+
+                <form class="contact100-form validate-form">
+                    <div class="wrap-input100 m-b-20 validate-input" data-validate="Email is required: ex@abc.xyz">
+                        <input class="s2-txt1 placeholder0 input100" type="text" name="email"
+                            placeholder="Email Address">
+                        <span class="focus-input100"></span>
+                    </div>
+
+                    <div class="w-full">
+                        <button class="flex-c-m s2-txt2 size4 bg1 bor1 hov1 trans-04">
+                            Subscribe
+                        </button>
+                    </div>
+                </form>
+
+                <p class="s2-txt3 p-t-18">
+                    We hate spam too! You can unsubcribe at anytime.
+                </p>
+            </div>
+
+            <div class="flex-w">
+                <a href="#" class="flex-c-m size5 bg3 how1 trans-04 m-r-5">
+                    <i class="fa fa-facebook"></i>
+                </a>
+
+                <a href="#" class="flex-c-m size5 bg4 how1 trans-04 m-r-5">
+                    <i class="fa fa-twitter"></i>
+                </a>
+
+                <a href="#" class="flex-c-m size5 bg5 how1 trans-04 m-r-5">
+                    <i class="fa fa-youtube-play"></i>
+                </a>
+            </div>
+        </div>
+    </div>
+
+
+
+
+
+    <!--===============================================================================================-->
+    <script src="vendor/jquery/jquery-3.2.1.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/bootstrap/js/popper.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/select2/select2.min.js"></script>
+    <!--===============================================================================================-->
+    <script src="vendor/countdowntime/moment.min.js"></script>
+    <script src="vendor/countdowntime/moment-timezone.min.js"></script>
+    <script src="vendor/countdowntime/moment-timezone-with-data.min.js"></script>
+    <script src="vendor/countdowntime/countdowntime.js"></script>
+    <script>
+        $('.cd100').countdown100({
+			/*Set Endtime here*/
+			/*Endtime must be > current time*/
+			endtimeYear: 0,
+			endtimeMonth: 0,
+			endtimeDate: 35,
+			endtimeHours: 18,
+			endtimeMinutes: 0,
+			endtimeSeconds: 0,
+			timeZone: ""
+			// ex:  timeZone: "America/New_York"
+			//go to " http://momentjs.com/timezone/ " to get timezone
+		});
+    </script>
+    <!--===============================================================================================-->
+    <script src="vendor/tilt/tilt.jquery.min.js"></script>
+    <script>
+        $('.js-tilt').tilt({
+			scale: 1.1
+		})
+    </script>
+    <!--===============================================================================================-->
+    <script src="/js/main.js"></script>
+
+</body>
+
 </html>
